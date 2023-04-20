8c89af2a4789051814b8b2f57b2637bb242da22c
Aniol Vergés Herrera <aniol.verges@students.salle.url.edu>
2023-04-20 11:02:37 +0200

    Acabado el sign in

--- src/main/persistance/UserDAO.java
+++ src/main/persistance/UserDAO.java
@@ -1,11 +1,13 @@
 package main.persistance;
 
-import main.business.entities.User;
+import src.main.business.entities.User;
 
 public interface UserDAO {
+
     boolean addUser(User user);
     User getUser(String userName, String pwd);
     boolean deleteUser(String user);
     boolean existsUser(String user);
     boolean existsEmail(String email);
+    User getUserMail(String userMail, String pwd);
 }
